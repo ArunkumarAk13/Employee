f2221224e5a85b8ce156cd1694b8eec3f3e56d1a
ArunkumarAk13 <arunkumar132004@gmail.com>
2025-04-04 12:17:44 +0530

    all addons added

--- app/components/add-employee.js
+++ app/components/add-employee.js
@@ -4,6 +4,7 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import 'ember-power-select/styles';
 
+
 export default class AddEmployeeComponent extends Component {
   @service employeeService;
   @service router;
@@ -12,13 +13,19 @@ export default class AddEmployeeComponent extends Component {
   countries = ['India','USA', 'Canada',  'Germany', 'France', 'Australia', 'Japan', 'Brazil', 'United Kingdom', 'South Africa'
   ];
   @tracked selectedCountry = null;
+  @tracked dob = null;
+
+  @action
+  setDOB([selectedDate]) {
+    this.dob = selectedDate;
+  }
 
   @action
   submitForm(event) {
     event.preventDefault();
 
     let name = event.target.name.value;
-    let dob = event.target.dob.value;
+    let dob = this.dob?.toISOString().split('T')[0]
     let country = this.selectedCountry;
 
     this.employeeService.addEmployee({ name, dob, country });

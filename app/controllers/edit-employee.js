9666e62d2d09161415cfe395a85939541c32a954
ArunkumarAk13 <arunkumar132004@gmail.com>
2025-04-14 12:44:57 +0530

    daterange  added

--- app/controllers/edit-employee.js
+++ app/controllers/edit-employee.js
@@ -5,6 +5,7 @@ import { action } from '@ember/object';
 import { task, timeout } from 'ember-concurrency';
 import 'ember-power-select/styles';
 import 'ember-pikaday/pikaday.css';
+import 'ember-power-calendar/styles';
 
 export default class EditEmployeeController extends Controller {
   @service employeeService;
@@ -19,14 +20,36 @@ export default class EditEmployeeController extends Controller {
   @tracked skillsPlaceholder = 'Select skills';
   @tracked collegePlaceholder = 'Select college';
   @tracked selectedCollege = null;
+  @tracked center = new Date('2016-05-17');
 
-  
+  @tracked range = {
+    start: null,
+    end: null,
+  };
+
+  @action
+  onCenterChange(selected) {
+    this.center = selected.date;
+  }
+
+  @action
+  onSelect(selected) {
+    if (selected.moment && selected.moment.start) {
+      this.range = {
+        start: selected.moment.start.toDate(),
+        end: selected.moment.end ? selected.moment.end.toDate() : null,
+      }}
+    };
 
   set model(model) {
     if (model) {
       this.index = model.index;
       this.employee = { ...model.employee };
       this.selectedCountry = this.employee.country;
+      if (this.employee.academicYear) {
+        this.range.start = new Date(this.employee.academicYear.from);
+        this.range.end = new Date(this.employee.academicYear.to);
+      }
     }
   }
 
@@ -88,8 +111,16 @@ export default class EditEmployeeController extends Controller {
   saveEmployee(event) {
     event.preventDefault();
     this.showSavingModal = true;
+    if (this.range.start && this.range.end) {
+      this.employee.academicYear = {
+        from: this.range.start.toLocaleDateString('en-CA'),
+        to: this.range.end.toLocaleDateString('en-CA')
+      };
+    }
+
     this.saveEmployeeTask.perform();
   }
+
   @action
   cancelSave() {
     this.saveEmployeeTask.cancelAll();
@@ -108,21 +139,19 @@ export default class EditEmployeeController extends Controller {
 
   @action
   updateGender(selectedGender) {
-  this.selectedGender = selectedGender;
-  this.employee.gender = selectedGender;
+    this.selectedGender = selectedGender;
+    this.employee.gender = selectedGender;
   }
 
   @action
   updateSkills(skills) {
-  this.selectedSkills = skills;
-  this.employee.skills = skills;
+    this.selectedSkills = skills;
+    this.employee.skills = skills;
   }
 
   @action
-updateCollege(selected) {
-  this.selectedCollege = selected;
-  this.employee.college = selected;
-}
-
-
+  updateCollege(selected) {
+    this.selectedCollege = selected;
+    this.employee.college = selected;
+  }
 }
